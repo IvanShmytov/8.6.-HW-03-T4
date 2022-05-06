89223cd884a2a8269978efa2cdbd3b808cf39cd7
IvanShmytov <ivanshmytov@mail.ru>
2022-05-06 17:39:57 +0300

    Код решения разделен на отдельные файлы, выделены классы.

--- Program.cs
+++ Program.cs
@@ -1,77 +1,18 @@
 ﻿using System;
-using System.Runtime.Serialization.Formatters.Binary;
-class MainClass
+
+partial class MainClass
 {
     public static void Main(string[] args)
     {
         string Dirname = @"C:\Users\ivans\Desktop\Students";
         Directory.CreateDirectory(Dirname);
-        BinaryFormatter form = new BinaryFormatter();
-        Student[] studs;
-        //using (FileStream fs = new FileStream("Students(4).dat", FileMode.OpenOrCreate))
-        //{
-        //    try
-        //    {
-        //        studs = (Student[])form.Deserialize(fs);
-        //        Console.WriteLine("объект десериализован");
-        //        foreach (var item in studs)
-        //        {
-        //            Console.WriteLine(item);
-        //        }
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        Console.WriteLine(e.GetType());
-        //    }
-        //}
+        Student[] students;
         studentdeserializer SD = new studentdeserializer();
-        studs = SD.DesertStuds("Students(4).dat");
-    }
-    [Serializable]
-    public class Student
-    {
-        public string Name { get; set; }
-        public string Group { get; set; }
-        public DateTime DateOfBirth { get; set; }
-
-        public Student(string name, string group, DateTime date)
-        {
-            Name = name;
-            Group = group;
-            DateOfBirth = date;
-        }
-        public override string ToString()
-        {
-            string result = $"имя - {Name}\nдата рождения - {DateOfBirth}\nгруппа - {Group}";
-            return result;
-        }
-    }
-    public class studentdeserializer
-    {
-        public Student[] DesertStuds(string filename) 
-        {
-            BinaryFormatter form = new BinaryFormatter();
-            Student[] studs = null;
-            using (FileStream fs = new FileStream(filename, FileMode.OpenOrCreate))
-            {
-                try
-                {
-                    studs = (Student[])form.Deserialize(fs);
-                    Console.WriteLine("объект десериализован");
-                    foreach (var item in studs)
-                    {
-                        Console.WriteLine(item);
-                    }
-                    
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.GetType());
-                }
-            }
-            return studs;
-        }
-
+        students = SD.DesertStuds("Students(4).dat");
+        //foreach (Student item in students)
+        //{
+        //    item.ReadStudToFile();
+        //}
     }
 
 }
---
+++ studentdeserializer.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+
+partial class MainClass
+{
+    public class studentdeserializer
+    {
+        public Student[] DesertStuds(string filename) 
+        {
+            BinaryFormatter form = new BinaryFormatter();
+            Student[] studs = null;
+            using (FileStream fs = new FileStream(filename, FileMode.OpenOrCreate))
+            {
+                try
+                {
+                    studs = (Student[])form.Deserialize(fs);
+                    Console.WriteLine("объект десериализован");
+                    foreach (var item in studs)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.GetType());
+                    Console.WriteLine(e.Message);
+                }
+            }
+            return studs;
+        }
+       
+
+    }
+
+}
